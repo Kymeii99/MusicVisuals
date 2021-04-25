1e62ccd3a0bb41f6827a662908d9c2a006d0eb16
Kim Thai <kimthai@Kims-MacBook-Pro.local>
2021-04-25 15:41:29 +0100

    Bridge + Sync to msuic

--- java/src/C19358373/BridgeScene.java
+++ java/src/C19358373/BridgeScene.java
@@ -18,12 +18,12 @@ public class BridgeScene {
         //The Sun
         kt.fill(255);
         kt.noStroke();
-        kt.circle(halfW,140,103);
+        kt.circle(halfW,140 + (kt.smothedAmplitude * 50),103 + (kt.smothedAmplitude * 50));
 
         //The Rays of the sun, gradient effect
         kt.fill(255,15);
         kt.noStroke();
-        kt.circle(halfW,140,113);
+        kt.circle(halfW,140 + (kt.smothedAmplitude * 50),113 + (kt.smothedAmplitude * 50));
 
         int sunrays = 0;
         
@@ -31,13 +31,20 @@ public class BridgeScene {
         {
             kt.fill(255,15);
             kt.noStroke();
-            kt.circle(halfW,140,123 + sunrays);
+            kt.circle(halfW,140 + (kt.smothedAmplitude * 50),123 + sunrays + (kt.smothedAmplitude * 50));
             sunrays += 10;
         }
-
+        
+        //Moutains
+        kt.fill(102,86,53);
+        kt.noStroke();
+        kt.triangle(0, halfH + 100 , 0, halfH - 100, halfW, halfH + 100);
+        kt.triangle(kt.width, halfH + 100 , kt.width, halfH - 100, halfW - 60, halfH + 100);
+        
         //Water
         kt.fill(0,119,182);
-        kt.rect(0, halfH + 100, kt.width, 200);
+        kt.rect(0, halfH + 100 - (kt.smothedAmplitude * 100), kt.width , 200 );
+        
 
         //Bridge
         kt.fill(255, 150);
@@ -45,7 +52,17 @@ public class BridgeScene {
 
         //Flooring of bridge
         kt.fill(0);
-        kt.rect(0, halfH + 50, kt.width, 10);    
+        kt.rect(0, halfH + 50, kt.width, 10); 
+        
+        //Line from the right and left of the bridge
+        kt.stroke(0);
+        kt.strokeWeight(2);
+        kt.line(0, halfH, 40, halfH + 50);
+
+        kt.stroke(0);
+        kt.strokeWeight(2);
+        kt.line(kt.width, halfH, kt.width - 40, halfH + 50);
+
 
     }
 }
